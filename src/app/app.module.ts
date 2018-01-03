831adc55b9b5b1224c27a547a28c4f1f09c500e5
Joseph Lesca <josephlesca@hotmail.com>
2018-01-03 10:45:39 -0800

    added proprty and event bnding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { FormsModule} from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
     ServerComponent,
     ServersComponent
   ],
   imports: [
-    BrowserModule
-    FormsModule
+    BrowserModule,
+    FormsModule,
     HttpModule
   ],
   providers: [],
